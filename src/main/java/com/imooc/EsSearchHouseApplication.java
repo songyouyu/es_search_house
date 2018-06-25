369bbd4cbd92746687d4deb61a918381fdfadb2c
songyouyu <songyy950208@163.com>
2018-06-25 14:42:28 +0800

    first init

---
+++ src/main/java/com/imooc/EsSearchHouseApplication.java
@@ -0,0 +1,17 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author youyusong
+ * @date 2018/06/25
+ */
+
+@SpringBootApplication
+public class EsSearchHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsSearchHouseApplication.class, args);
+    }
+}
